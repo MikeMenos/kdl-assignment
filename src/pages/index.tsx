709f9a9946654dd6cd43a70f186e530bb646ecd1
Mike Menos <mikemenos95@gmail.com>
2023-10-16 01:02:39 +0300

    comment removal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,6 @@ export default function Home() {
     },
     {
       onMutate: () => {
-        // Show the delete confirmation modal
         setDeleteModalOpen(true);
       },
       onSuccess: () => {
